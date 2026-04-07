e927499b8042cf5beeca975f7f9ad4ce7e92b50b
Issei <rn0c.1iarn9@gmail.com>
2026-04-07 23:06:17 +0900

    初回アップロード

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", e=>{
+  e.waitUntil(
+    caches.open("app").then(cache=>cache.addAll(["./","./index.html"]))
+  );
+});
+
+self.addEventListener("fetch", e=>{
+  e.respondWith(
+    caches.match(e.request).then(res=>res||fetch(e.request))
+  );
+});
